b8c95641732e5722822ae882157ba76d12ba164b
Nicolas <nicolas.nowinski@gmail.com>
2021-10-08 18:44:53 +0200

    Ajout du controler et de tests

---
+++ rest/src/main/java/fr/nowinski/fizzbuzz/rest/controller/FizzBuzzController.java
@@ -0,0 +1,56 @@
+package fr.nowinski.fizzbuzz.rest.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.nowinski.fizzbuzz.commons.dto.PageDto;
+import fr.nowinski.fizzbuzz.commons.dto.StatisticDto;
+import fr.nowinski.fizzbuzz.commons.services.FizzBuzzService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+public class FizzBuzzController {
+
+	@Autowired
+	private FizzBuzzService fizzService;
+
+	@Operation(summary = "Get a list of strings with numbers from 1 to limit, where: all multiples of int1 are replaced by str1, all multiples of int2 are replaced by str2, all multiples of int1 and int2 are replaced by str1str2")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "List of numbers and strings", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = List.class), examples = {
+					@ExampleObject(description = "1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz,16,...") }) }) })
+	@GetMapping("/fizzbuzz")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Object> getFizzBuzz(@RequestParam(defaultValue = "3", required = true) Integer int1,
+			@RequestParam(defaultValue = "5", required = true) Integer int2,
+			@RequestParam(defaultValue = "fizz", required = true) String str1,
+			@RequestParam(defaultValue = "buzz", required = true) String str2,
+			@RequestParam(defaultValue = "100", required = true) Integer limit) {
+
+		final PageDto page = new PageDto(int1, int2, str1, str2, limit);
+
+		return fizzService.getFizzBuzzList(page);
+
+	}
+
+	@Operation(summary = "Get the parameters corresponding to the most used request, as well as the number of hits for this request")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "The most frequent request has been", content = {
+					@Content(mediaType = "application/json") }) })
+	@GetMapping("/statistics")
+	@ResponseStatus(HttpStatus.OK)
+	public List<StatisticDto> getStatistic() {
+		return this.fizzService.getStatistics();
+	}
+
+}
---
+++ rest/src/test/java/fr/nowinski/fizzbuzz/rest/controller/FizzBuzzControllerTest.java
@@ -0,0 +1,84 @@
+package fr.nowinski.fizzbuzz.rest.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import fr.nowinski.fizzbuzz.commons.dto.PageDto;
+import fr.nowinski.fizzbuzz.commons.dto.StatisticDto;
+import fr.nowinski.fizzbuzz.commons.services.FizzBuzzService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class FizzBuzzControllerTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@MockBean
+	private FizzBuzzService fizzBuzzService;
+
+	@Captor
+	private ArgumentCaptor<PageDto> pageCaptor;
+
+	private String baseUrl;
+
+	@BeforeEach
+	public void setUp() {
+		baseUrl = String.format("http://localhost:%d/", port);
+	}
+
+	@Test
+	void test_statistics() {
+		final StatisticDto stat = new StatisticDto();
+		stat.setStr1("toto");
+		stat.setNumberOfHits(5);
+		final List<StatisticDto> stats = Arrays.asList(stat);
+		Mockito.when(fizzBuzzService.getStatistics()).thenReturn(stats);
+
+		ResponseEntity<List<StatisticDto>> response = restTemplate.exchange(baseUrl + "statistics", HttpMethod.GET,
+				null, new ParameterizedTypeReference<List<StatisticDto>>() {
+				});
+
+		Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+		Assertions.assertEquals(stats.size(), response.getBody().size());
+		Assertions.assertEquals(stats.get(0).getStr1(), response.getBody().get(0).getStr1());
+		Assertions.assertEquals(stats.get(0).getNumberOfHits(), response.getBody().get(0).getNumberOfHits());
+	}
+
+	@Test
+	void test_fizzbuzz() {
+		final List<Object> fizzbuzz = Arrays.asList("1", 2);
+		Mockito.when(fizzBuzzService.getFizzBuzzList(Mockito.any(PageDto.class))).thenReturn(fizzbuzz);
+
+		ResponseEntity<List<Object>> response = restTemplate.exchange(
+				baseUrl + "fizzbuzz?int1=4&int2=5&str1=fizz&str2=toto&limit=102", HttpMethod.GET, null,
+				new ParameterizedTypeReference<List<Object>>() {
+				});
+
+		Mockito.verify(fizzBuzzService).getFizzBuzzList(pageCaptor.capture());
+		Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+		Assertions.assertEquals(4, pageCaptor.getValue().getInt1());
+		Assertions.assertEquals("toto", pageCaptor.getValue().getStr2());
+		Assertions.assertEquals(102, pageCaptor.getValue().getLimit());
+	}
+
+}
